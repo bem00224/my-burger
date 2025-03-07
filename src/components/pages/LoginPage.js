633cf662fae2a55d2225daa908a666c9e3b474b8
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-07 10:08:14 +0100

    ✨ F01: LoginPage > Gestion du changement dans "input"

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 const LoginPage = () => {
+    //state
+    const [ username, setUsername] = useState("")
+    //comportements
+    const handleChange = (e) => {
+        setUsername(e.target.value)
+    }
+    //d'affichage
     return ( 
         <div>
             <h1>Bienvenue chez nous !</h1><br/><br/>
             <h3>Connectez vous</h3>
             <form>
-                <input type="text" placeholder="Entrez votre prénom..." required />
+                <input type="text" placeholder="Entrez votre prénom..." value={username} onChange={handleChange} required />
                 <button>Accéder à votre espace</button>
             </form>
         </div>
