522e97722c27309686f43e4be6a33694dadd0f31
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-07 13:53:39 +0100

    ✨ F02: order > ErrorPage > Add Link to return HomePage

--- src/components/pages/error/ErrorPage.js
+++ src/components/pages/error/ErrorPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ErrorPage() {
   return (
     <div>
       <h1>ErrorPage</h1>
+      <button><Link to="/">Retourner vers la page d'accueil</Link></button>
     </div>
   )
 }
