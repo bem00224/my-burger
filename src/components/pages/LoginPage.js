1a71862ebd6904438f13fbbd70884acba226060e
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-07 10:19:15 +0100

    ✨ F01: LoginPage > Gestion de la soumission du formulaire

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -2,17 +2,23 @@ import { useState } from "react";
 
 const LoginPage = () => {
     //state
-    const [ username, setUsername] = useState("")
+    const [ username, setUsername] = useState("bob")
     //comportements
     const handleChange = (e) => {
         setUsername(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(`Bonjour ${username}`)
+        setUsername("")
     }
     //d'affichage
     return ( 
         <div>
             <h1>Bienvenue chez nous !</h1><br/><br/>
             <h3>Connectez vous</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Entrez votre prénom..." value={username} onChange={handleChange} required />
                 <button>Accéder à votre espace</button>
             </form>
