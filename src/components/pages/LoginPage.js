688e95ab100e3f9eababa33e954d5bab4e34abef
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-07 10:01:56 +0100

    ✨ F01: LoginPage > Add Form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -3,6 +3,10 @@ const LoginPage = () => {
         <div>
             <h1>Bienvenue chez nous !</h1><br/><br/>
             <h3>Connectez vous</h3>
+            <form>
+                <input type="text" placeholder="Entrez votre prénom..." required />
+                <button>Accéder à votre espace</button>
+            </form>
         </div>
      );
 }
