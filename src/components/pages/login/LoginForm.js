323df4ab9d700866f837df8f9929c1b8a5daefa2
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-07 20:38:44 +0100

    ♻️ F03: LOGIN > LoginForm > Create reusable <Input/> and <Button/>

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Input from "../../reusable-ui/Input";
+import Button from "../../reusable-ui/Button";
+import { IoChevronForward } from "react-icons/io5"
+import { BsPersonCircle } from "react-icons/bs"
+import { theme } from "../../../theme";
 
 const LoginForm = () => {
     //state
@@ -19,13 +25,54 @@ const LoginForm = () => {
      
     //d'affichage
     return ( 
-        <form onSubmit={handleSubmit}>
-            <h1>Bienvenue chez nous !</h1><br/><br/>
-            <h3>Connectez vous</h3>
-            <input type="text" placeholder="Entrez votre prénom..." value={username} onChange={handleChange} required />
-            <button>Accéder à votre espace</button>
-        </form>
+        <LoginFormStyled onSubmit={handleSubmit}>
+            <div>
+               <h1>Bienvenue chez nous !</h1><hr/>
+               <h2>Connectez-vous</h2>
+            </div>
+            <div>
+               <Input type="text" 
+                  placeholder="Entrez votre prénom" 
+                  value={username} 
+                  onChange={handleChange} 
+                  Icon={<BsPersonCircle className="icon" />} 
+                  required 
+               />
+               <Button label={"Accéder à mon espace"} Icon={<IoChevronForward className="icon" />} />
+            </div>
+        </LoginFormStyled>
      );
 }
  
 export default LoginForm;
+
+const LoginFormStyled = styled.form`
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 40px ${theme.spacing.lg};
+  border-radius: ${theme.borderRadius.round};
+  font-family: "Amatic SC", cursive;
+  hr {
+    border: 1.5px solid ${theme.colors.loginLine};
+    margin-bottom: ${theme.gridUnit * 5}px;
+  }
+  h1 {
+    color: ${theme.colors.white};
+    font-size: ${theme.fonts.size.P5};
+  }
+
+  h2 {
+    margin: 20px 10px 10px;
+    color: ${theme.colors.white};
+    font-size: ${theme.fonts.size.P4};
+  }
+  .icon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: ${theme.fonts.size.P0};
+    margin-left: 10px;
+  }
+`;
