8afd879081858a01dcf93aa93e9002e39b1bd0f8
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-07 14:54:22 +0100

    ✨ F02: App> changer les '' en "" pour le chemin path

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/pages/login/LoginPage';
 import ErrorPage from './components/pages/error/ErrorPage';
+import OrderPage from './components/pages/order/OrderPage'
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path='/' exact element ={<LoginPage />} />
-      <Route path='*' element = {<ErrorPage/> } />
+      <Route path="/" exact element ={<LoginPage />} />
+      <Route path="/order/:username" element={<OrderPage/>} />
+      <Route path="*" element = {<ErrorPage/> } />
     </Routes>
     </>
   );
