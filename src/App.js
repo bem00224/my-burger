981e7dcc556dd4e680773a5029bdc7dad53eb852
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-07 13:51:34 +0100

    ✨ F02: order > Install Routing systeme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/pages/login/LoginPage';
+import ErrorPage from './components/pages/error/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage />
-    </div>
+    <>
+    <Routes>
+      <Route path='/' exact element ={<LoginPage />} />
+      <Route path='*' element = {<ErrorPage/> } />
+    </Routes>
+    </>
   );
 }
 
