cba6b5c2086bd876c90e8130d9fd3ea8be702508
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-12 14:47:45 +0100

    ✨ F04: ORDER > Navbar > trigger reload on click

--- src/components/pages/order/navbar/Navbar.js
+++ src/components/pages/order/navbar/Navbar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import NavbarRight from './NavbarRight';
 import Logo from '../../../reusable-ui/Logo';
+import { refreshPage } from '../../../../utils/windows';
+import { theme } from '../../../../theme';
 
 const Navbar = () => {
     return (
         <NavbarStyled>
-            <Logo className="logo"/>
+            <Logo className="logo-order-page" onClick={() => refreshPage()}/>
             <NavbarRight/>
         </NavbarStyled>
     );
@@ -15,13 +17,17 @@ const Navbar = () => {
 export default Navbar;
 
 const NavbarStyled = styled.div`
-  border: 1px solid blue;
+  background: ${theme.colors.white};
+  height: 10vh;
   display: flex;
-  align-items: center;
   justify-content: space-between;
   padding: 0 20px;
 
-  .logo {
-    transform: scale(1);
+  border-top-left-radius: ${theme.borderRadius.extraRound};
+  border-top-right-radius: ${theme.borderRadius.extraRound};
+  /* align-items: center; */
+
+  .logo-order-page {
+    cursor: pointer;
   }
 `;
