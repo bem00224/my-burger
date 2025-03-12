add2a553c4212d2a4339b9952646b66227b73204
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-12 21:30:10 +0100

    F05:  Card > Add Layout

---
+++ src/components/reusable-ui/Cards.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function Cards() {
+  return (
+    <CardStyled className="produit">
+      <div className="image"></div>
+      <div className="text-info"></div>
+    </CardStyled>
+  )
+}
+
+const CardStyled = styled.div`
+  
+`;
