1f14c0616347c7904f60709c7347cf0e9bbf268d
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-12 14:18:27 +0100

    💄 F04: OrderPage > Add style

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -2,18 +2,34 @@ import React from 'react';
 import Navbar from './navbar/Navbar';
 import Main from './main/Main';
 import styled from 'styled-components';
+import { theme } from '../../../theme';
 
 const OrderPage = () => {
     
     return (
         <OrderPageStyled>
-            <Navbar />
-            <Main/>
+            <div className="container">
+                <Navbar />
+                <Main/>
+            </div>
         </OrderPageStyled>
     );
 }
 
 export default OrderPage;
 const OrderPageStyled = styled.div`
-  border: 1px solid gray;
+  background: ${theme.colors.primary};
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background: red;
+    height: 95vh;
+    width: 1400px;
+    display: flex;
+    flex-direction: column;
+    border-radius: ${theme.borderRadius.extraRound};
+  }
 `;
