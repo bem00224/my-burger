688f75be932ff5c803b44e9f45845153bbade057
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-13 02:28:28 +0100

    ♻️  F05: ORDER > Menu > Replace price to leftDescription

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -1,22 +1,34 @@
 import React, { useState } from 'react'
 import Cards from '../../../reusable-ui/Cards'
-import { fakeMenu1 } from '../../../../fakeData/fakeMenu'
+import { fakeMenu2 } from '../../../../fakeData/fakeMenu'
+import { formatPrice } from '../../../../utils/maths'
+import styled from 'styled-components'
+import { theme } from '../../../../theme'
 
 export default function Menu() {
-    const [ menu, setMenu ] = useState(fakeMenu1)
+    const [ menu, setMenu ] = useState(fakeMenu2)
 
   return (
-    <div>
+    <MenuStyled className="menu">
       {menu.map(({id, imageSource,price,title}) => {
         return (
             <Cards 
                 key={id}
-                imageSource={imageSource}
-                price={price}
                 title={title}
+                imageSource={imageSource}
+                leftDescription={formatPrice(price)}
             />
         )
       } )}
-    </div>
+    </MenuStyled>
   )
 }
+const MenuStyled = styled.div`
+  background: ${theme.colors.background_white};
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-row-gap: 60px;
+  padding: 50px 50px 150px;
+  justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+`
