f17c2f28964f9e417647989f03f8f5f83f16b6bc
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-13 01:59:47 +0100

    ✨ F05: ORDER > Menu > Replace Product

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Cards from '../../../reusable-ui/Cards'
+import { fakeMenu1 } from '../../../../fakeData/fakeMenu'
 
 export default function Menu() {
+    const [ menu, setMenu ] = useState(fakeMenu1)
+
   return (
     <div>
-      
+      {menu.map(({id, imageSource,price,title}) => {
+        return (
+            <Cards 
+                key={id}
+                imageSource={imageSource}
+                price={price}
+                title={title}
+            />
+        )
+      } )}
     </div>
   )
 }
