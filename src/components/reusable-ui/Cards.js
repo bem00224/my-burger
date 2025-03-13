c08eb033c0eb404166590145692064e6be8437c4
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-13 01:58:20 +0100

    ✨ F05: ORDER > Menu > Create Product

--- src/components/reusable-ui/Cards.js
+++ src/components/reusable-ui/Cards.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled from 'styled-components';
+import Button from './Button';
 
-export default function Cards() {
+export default function Cards({imageSource,title, leftDescription}) {
   return (
     <CardStyled className="produit">
-      <div className="image"></div>
-      <div className="text-info"></div>
+      <div className="image">
+        <img src={imageSource} alt={title} />
+      </div>
+      <div className="text-info">
+        <div className="title">{title}</div>
+        <div className="description">
+        <div className="left-description">{leftDescription}</div>
+        <div className="right-description">
+          <Button className="primary-button" label={"Ajouter"} />
+        </div>
+        </div>
+      </div>
     </CardStyled>
   )
 }
