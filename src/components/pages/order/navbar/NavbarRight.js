621fb72ec208fe9a22c4e59f60c337d8a0ffa40e
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-14 01:44:37 +0100

    ✨ F06: NAVBAR > Add notification toast on admin button click

--- src/components/pages/order/navbar/NavbarRight.js
+++ src/components/pages/order/navbar/NavbarRight.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import Profile from './Profile';
+import ToggleButton from '../../../reusable-ui/ToggleButton';
+import { toast } from 'react-toastify';
+import ToastAdmin from './ToastAdmin';
 
 const NavbarRight = () => {
     const { username } = useParams()
+    const [isModeAdmin, setIsModeAdmin] = useState(false)
+
+  const displayToastNotification = () => {
+    if (!isModeAdmin) {
+      toast.info("Mode admin activé", {
+        // icon: <FaUserSecret size={30} />,
+        theme: "dark",
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      })
+    }
+    setIsModeAdmin(!isModeAdmin)
+  }
 
     return (
         <NavbarRightStyled>
-            {/* <div className="admin-button">Admin Button</div> */}
+            <ToggleButton
+              labelIfUnchecked="ACTIVER LE MODE ADMIN"
+              labelIfChecked="DÉSACTIVER LE MODE ADMIN"
+              onToggle={displayToastNotification}
+            />
             <Profile username={username} />
+            <ToastAdmin />
         </NavbarRightStyled>
     );
 }
@@ -20,12 +46,4 @@ const NavbarRightStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
-
-  /* .admin-button {
-    background: lightblue;
-  } */
-
-  .profile {
-    background: yellow;
-  }
 `;
