61647e8eae32b6634ae94cb44fda81393976853c
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-18 08:25:20 +0100

    ✨ F07:  POSITIONNER le context provider avec la value

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './navbar/Navbar';
 import Main from './main/Main';
 import styled from 'styled-components';
 import { theme } from '../../../theme';
+import OrderContext from '../../../context/OrderContext';
 
 const OrderPage = () => {
+  //state
+  const [isModeAdmin, setIsModeAdmin] = useState(false)
+  //comportements
+  const orderContextValue = {
+    isModeAdmin,
+    setIsModeAdmin
+  }
+  //afichage
     
     return (
         <OrderPageStyled>
-            <div className="container">
-                <Navbar />
-                <Main/>
-            </div>
+          <div className="container">
+            <OrderContext.Provider value={orderContextValue}>
+              <Navbar />
+              <Main/>
+            </OrderContext.Provider>
+          </div>
         </OrderPageStyled>
     );
 }
