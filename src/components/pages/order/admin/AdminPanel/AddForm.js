52f267778104402bc80b827f5446a2f4e6caf011
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-28 16:29:50 +0100

    🚧 F08: ADMIN > Panel > Add AddForm and EditForm

---
+++ src/components/pages/order/admin/AdminPanel/AddForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function AddForm() {
+  return (
+    <AddFormStyled>
+      AddForm
+    </AddFormStyled>
+  )
+}
+const AddFormStyled = styled.form`
+  border: 2px solid black;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  height: 100%;
+  width: 70%;
+
+  .image-preview {
+    background: red;
+    grid-area: 1 / 1 / 4 / 2;
+  }
+
+  .input-fields {
+    background: blue;
+    grid-area: 1 / 2 / -2 / 3;
+
+    display: grid;
+  }
+
+  .submit-button {
+    background: green;
+    grid-area: 4 / -2 / -1 / -1;
+    width: 50%;
+  }
+`;
