afc531db8b81dff3b0e18f6e65c99a5609135518
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-03-28 16:30:54 +0100

    🚧 F08: ADMIN > Panel > Change label to content

---
+++ src/components/pages/order/admin/AdminPanel/AdminPanel.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components';
+import { theme } from '../../../../../theme';
+import OrderContext from '../../../../../context/OrderContext';
+import { tabsConfig, getTabSelected } from '../tabsConfig';
+
+export default function AdminPanel() {
+  const {currentTabSelected} = useContext(OrderContext)
+  const tabs = tabsConfig
+  const tabSelected = getTabSelected(tabs,currentTabSelected)
+  return (
+    <AdminPanelStyled>
+      <div>{ tabSelected.content }</div>
+    </AdminPanelStyled>
+  )
+}
+const AdminPanelStyled = styled.div`
+  height: 251px;
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  background: ${theme.colors.white};
+  border-top: 1px solid ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
+`;
