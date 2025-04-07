d62b770fc16960980d76660e4cf55e1e5deaebff
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-04-07 05:20:58 +0100

    ♻️ F08: Main > Add MainRightSide

--- src/components/pages/order/main/Main.js
+++ src/components/pages/order/main/Main.js
@@ -1,23 +1,20 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
-import Menu from './Menu';
+import Menu from './MainRightSide/Menu/Menu';
 import { theme } from '../../../../theme/index'
 import Admin from '../admin/Admin';
 import OrderContext from '../../../../context/OrderContext';
+import MainRightSide from './MainRightSide/MainRightSide';
 
 const Main = () => {
   //state
-  const { isModeAdmin } = useContext(OrderContext)
   //comportements
   //affichage
   
     return (
         <MainStyled>
             {/* <div className="basket">Basket</div> */}
-            <div className="menu-and-admin">
-              <Menu />
-              {isModeAdmin ? (<Admin/>) : "" }
-            </div>
+            <MainRightSide/>
         </MainStyled>
     );
 }
@@ -26,24 +23,12 @@ export default Main;
 
 const MainStyled = styled.div`
   background: ${theme.colors.background_white};
-  flex: 1; // or you can also use this : height: calc(95vh - 10vh);
   height: calc(95vh - 10vh);
 
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
-  box-shadow: ${theme.shadows.strong};
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   display: grid;
   grid-template-columns: 1fr;
-
-  .basket {
-    background: pink;
-    border: 1px solid black;
-  }
-
-  .menu-and-admin {
-    position: relative;
-    display: grid;
-    overflow-y: hidden;
-  }
 `
---
+++ src/components/pages/order/main/MainRightSide/MainRightSide.js
@@ -0,0 +1,25 @@
+import { useContext } from "react"
+import styled from "styled-components"
+import Menu from "./Menu/Menu"
+import OrderContext from "../../../../../context/OrderContext"
+import { theme } from "../../../../../theme"
+import Admin from "../../admin/Admin"
+
+export default function MainRightSide() {
+  const { isModeAdmin } = useContext(OrderContext)
+
+  return (
+    <MainRightSideStyled>
+      <Menu />
+      {isModeAdmin && <Admin />}
+    </MainRightSideStyled>
+  )
+}
+
+const MainRightSideStyled = styled.div`
+  position: relative;
+  overflow-y: hidden;
+  display: grid;
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
+`
