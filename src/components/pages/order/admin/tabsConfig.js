fc6d436ce31be03bb14545fd8114c94bd10e4cdd
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-05-02 22:10:39 +0100

    🔥 F09 : ADMIN PANEL > Remove examples

--- src/components/pages/order/admin/tabsConfig.js
+++ src/components/pages/order/admin/tabsConfig.js
@@ -3,9 +3,6 @@ import { MdModeEditOutline } from "react-icons/md";
 import AddForm from "./AdminPanel/AddForm";
 import EditForm from "./AdminPanel/EditForm";
 import HintMessage from "./AdminPanel/HintMessage";
-import { FiSettings } from "react-icons/fi";
-import Button from "../../../reusable-ui/Button";
-import { Form } from "react-router-dom";
 
 export const getTabsConfig= (hasAlreadyBeenClicked) => [
     {
@@ -20,32 +17,7 @@ export const getTabsConfig= (hasAlreadyBeenClicked) => [
       label: "Modifier un produit",
       content: hasAlreadyBeenClicked ? <EditForm/> : <HintMessage />,
     },
-    {
-      index: "manger",
-      label: "Autre onglet",
-      Icon: <FiSettings />,
-      Content: <UnComposant />,
-    },
 ]
 
 export const getTabSelected = (tabs,currentTabSelected) => tabs.find((tab) => tab.index === currentTabSelected)
 
-export function UnComposant() {
-  return (
-    <Form
-      product={{
-        id: "1",
-        imageSource: "/images/burger1.png",
-        title: "Burger 1",
-        price: 5.297,
-        quantity: 0,
-        isAvailable: true,
-        isAdvertised: false,
-      }}
-    >
-      <div style={{ width: "50%" }}>
-        <Button version="normal" label={"Bonjour"} />
-      </div>
-    </Form>
-  )
-}
