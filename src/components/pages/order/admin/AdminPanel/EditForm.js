4b198a9adc9ae2ed50ee7c652eaea6b4ce3cd1fe
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-05-02 16:23:43 +0100

    🚧  F09 : ADMIN > EditForm > Retrieve info of Card clicked on with form from AddForm

--- src/components/pages/order/admin/AdminPanel/EditForm.js
+++ src/components/pages/order/admin/AdminPanel/EditForm.js
@@ -1,13 +1,61 @@
-import React, { useContext } from 'react'
-import HintMessage from './HintMessage'
+import React, { useContext, useState } from 'react'
 import OrderContext from '../../../../../context/OrderContext'
+import styled from 'styled-components'
+import ImagePreview from './ImagePreview'
+import { getInputTextsConfig } from './inputTextsConfig'
+import Input from '../../../../reusable-ui/Input'
+import { EMPTY_PRODUCT } from '../../../../../enums/product'
+
 
 export default function EditForm() {
   const {productSelected} = useContext(OrderContext)
+  const inputTexts = getInputTextsConfig(productSelected)
+  const [productBeingEdited, setProductBeingEdited] = useState(EMPTY_PRODUCT)
+  const handleChange = (e) => {
+    setProductBeingEdited({...productBeingEdited, [e.target.name]:e.target.value})
+  }
+
   return (
-    <div>
-      <HintMessage/>
-      <span>{productSelected && productSelected.title}</span>
-    </div>
+    <EditFormStyled>
+      <ImagePreview imageSource={productSelected.imageSource} title={productSelected.title} />
+      <div className="input-fields">
+        {inputTexts.map((input) => (
+          <Input {...input} key={input.id} onChange={handleChange} version="minimalist" />
+        ))}
+      </div>
+      <div className="submit"></div>
+    </EditFormStyled>
   )
 }
+const EditFormStyled = styled.form`
+  /* border: 2px solid black; */
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  height: 100%;
+  width: 70%;
+  grid-column-gap: 20px;
+  grid-row-gap: 8px;
+
+  .input-fields {
+    /* background: blue; */
+    grid-area: 1 / 2 / -2 / 3;
+
+    display: grid;
+    grid-row-gap: 8px;
+  }
+
+  .submit {
+    /* background: green; */
+    grid-area: 4 / -2 / -1 / -1;
+    display: flex;
+    align-items: center;
+    position: relative;
+    top: 3px;
+
+    .submit-button {
+      /* width: 50%; */
+      height: 100%;
+    }
+  }
+`
