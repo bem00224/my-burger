e3da299392f623839c5d126b0baf574d7a4ff2a8
Elhadj Mamadou Bah <bahe95578@gmail.com>
2025-05-03 17:22:34 +0100

    ♻️ F11: BASKET > Add Product > Create incrementProductAlreadyInBasket

--- src/utils/array.js
+++ src/utils/array.js
@@ -5,3 +5,7 @@ export const deepClone = (array) => {
 export const find = (id, array) => {
     return array.find((itemInArray) => itemInArray.id === id)
 }
+
+export const findIndex = (idWithUnknowwIndex, array) => {
+    return array.findIndex((itemInArray) => itemInArray.id === idWithUnknowwIndex)
+}
